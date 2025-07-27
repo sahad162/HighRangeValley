d4aa6a66a1213cf9e756e6d19f112fc56037e3ee
sahad162 <muhammedsahad162@gmail.com>
2025-07-27 08:54:36 +0530

    HIghn range 4 sections with responsive

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Connect from '../components/LandingPage/Connect'
+import Header from '../components/Header'
+import Suppliers from '../components/LandingPage/Suppliers'
+function LandingPage() {
+  return (
+    <>
+    < Header/>
+    < Suppliers/>
+    <Connect/>
+    < Footer/>  
+    </>
+  )
+}
+
+export default LandingPage
